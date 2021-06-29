bea3973c2ce7a08498fd754d46d1604673289e59
Punamkumari03 <punamgupta250602@gmail.com>
2021-06-29 18:17:23 +0530

    Bulb_on_off

---
+++ script.js
@@ -0,0 +1,10 @@
+// turn on
+const turnOn = () =>
+{
+    let imgOb = document.getElementById("bulb");
+    imgOb.setAttribute("src","on.jpg");
+};
+const turnOff = () =>{
+let imgOb = document.getElementById("bulb");
+imgOb.setAttribute("src", "off.jpg");
+};
